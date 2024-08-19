1437bc9da03c830ec7d7b8f63479506f7686481e
DESKTOP-5OBM98S\SEOULIT <Cathedral8293@gmail.com>
2024-08-19 17:44:32 +0900

    no message

--- swipecar/Assets/Scripts/CarController.cs
+++ swipecar/Assets/Scripts/CarController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    // 속도 변수
     public float translateSpeed;
+    // 좌클릭시 좌표
     public Vector3 mousePosition1;
+    // 우클릭시 좌표
     public Vector3 mousePosition2;
+    // 우클릭 좌표-좌클릭 좌표 : 좌표 사이의 거리
     public Vector3 swiperate;
     void Start()
     {
@@ -16,14 +20,16 @@ public class CarController : MonoBehaviour
 
     void Update()
     {
-
+        // bool 변슈 좌클릭 우클릭시
         bool isDown = Input.GetMouseButtonDown(0);
         bool isUp = Input.GetMouseButtonUp(0);
+        // 좌클릭시 좌표 저장
         if (isDown)
         {
             mousePosition1 = Input.mousePosition;
             Debug.Log(mousePosition1);
         }
+        // 우클릭시 좌표 저장 및 좌표사이의 거리, 속도 저장
         else if (isUp)
         {
             mousePosition2 = Input.mousePosition;
@@ -31,7 +37,9 @@ public class CarController : MonoBehaviour
             swiperate = mousePosition2 - mousePosition1;
             translateSpeed = swiperate.x * 0.002f;
         }
+        // 이동
         this.transform.Translate(translateSpeed, 0, 0);
+        // 이동속도 감소
         translateSpeed *= 0.96f;
     }
 }
